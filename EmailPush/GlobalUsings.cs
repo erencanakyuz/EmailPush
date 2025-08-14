4e2eb7cca013380c35fef363b28029188b211d26
erencanakyuz <erencakyuz@gmail.com>
2025-08-14 19:33:03 +0300

    Uncessary dependcy clean up

--- EmailPush/GlobalUsings.cs
+++ EmailPush/GlobalUsings.cs
@@ -1,7 +1,4 @@
 global using EmailPush.Domain.Entities;
 global using EmailPush.Domain.Interfaces;
 global using EmailPush.Domain.Messages;
-global using EmailPush.Infrastructure.Data;
-global using EmailPush.Infrastructure.Repositories;
 global using Microsoft.EntityFrameworkCore;
-global using MassTransit;
